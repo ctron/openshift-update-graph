22de50794997a27b8708a073fc0ac716668a4b94
Jens Reimann <jreimann@redhat.com>
2020-08-06 11:56:50 +0200

    store expanded in pretty-print, add nodes as well

--- .github/workflows/expand.js
+++ .github/workflows/expand.js
@@ -4,18 +4,26 @@ const data = fs.readFileSync(0, 'utf-8');
 
 const channel = JSON.parse(data)
 
-let result = [];
+let result = {
+    nodes: [],
+    edges: []
+};
+
+channel.nodes.forEach(function(node){
+    result.nodes.push(node.version);
+})
+result.nodes.sort((a,b) => a.localeCompare(b));
 
 channel.edges.forEach(function(edge){
     let from = channel.nodes[edge[0]];
     let to = channel.nodes[edge[1]];
-    result.push({
+    result.edges.push({
         from: from.version,
         to: to.version,
     });
 });
 
-result.sort((a,b) => {
+result.edges.sort((a,b) => {
     let c = a.from.localeCompare(b.from);
     if (c !== 0) {
         return c;
@@ -24,4 +32,4 @@ result.sort((a,b) => {
     }
 });
 
-process.stdout.write(JSON.stringify(result))
+process.stdout.write(JSON.stringify(result, null, "  "))
