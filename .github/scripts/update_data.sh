a835ffe6780148bd8b3adaa64bc2ed63d73df367
Jens Reimann <jreimann@redhat.com>
2020-08-06 11:24:57 +0200

    Handle detection of new stream

--- .github/scripts/update_data.sh
+++ .github/scripts/update_data.sh
@@ -10,12 +10,19 @@ for i in $(cat streams.json | jq -r .[]); do
   mkdir -p streams/expanded
   cat "streams/${i}.json" | node .github/workflows/expand.js > "streams/expanded/${i}.json"
 
-  git --no-pager diff -- "streams/expanded/${i}.json"
+  echo "---"
+  git --no-pager diff -- "streams/expanded/${i}.json" || true
+  echo "---"
 
-  if git diff --exit-code -- "streams/expanded/${i}.json"; then
-    echo "${i}: Update model did not change, reverting JSON ..."
-    git checkout -- "streams/expanded/${i}.json"
-    git checkout -- "streams/${i}.json"
-  fi
+  # new file -> leave it
+  git ls-files -o --error-unmatch -- "streams/${i}.json" &>/dev/null && continue
+
+  # normalized file did change -> leave it
+  git diff --exit-code -- "streams/expanded/${i}.json" || continue
+
+  # not a new file, and no change in normalized file -> revert to existing version
+  echo "${i}: Update model did not change, reverting JSON ..."
+  git checkout -- "streams/expanded/${i}.json"
+  git checkout -- "streams/${i}.json"
 
 done
