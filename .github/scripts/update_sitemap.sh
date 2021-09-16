d47341599cd0f03e1e9c384d7e38040bed5a542d
Jens Reimann <jreimann@redhat.com>
2021-09-16 15:12:24 +0200

    Generate sitemap

---
+++ .github/scripts/update_sitemap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo '<?xml version="1.0" encoding="utf-8"?>' > sitemap.xml
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> sitemap.xml
+
+echo '<url><loc>https://ctron.github.io/openshift-update-graph</loc></url>' >> sitemap.xml
+
+jq < streams.json -r '.[] | "<url><loc>https://ctron.github.io/openshift-update-graph/#\(.)</loc></url>"' >> sitemap.xml
+
+echo '</urlset>' >> sitemap.xml
+
+cat sitemap.xml
