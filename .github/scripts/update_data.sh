cd10c034cb9627cf369db433ab30eaacfb1c9333
Jens Reimann <jreimann@redhat.com>
2021-10-20 10:50:28 +0200

    verbose curl

--- .github/scripts/update_data.sh
+++ .github/scripts/update_data.sh
@@ -6,7 +6,7 @@ for i in $(cat streams.json | jq -r .[]); do
 
   echo Updating: "${i}"
 
-  ( set -x; curl -sH 'Accept:application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=${i}&arch=amd64" -o "streams/${i}.json" )
+  ( set -x; curl -vsH 'Accept:application/json' "https://api.openshift.com/api/upgrades_info/v1/graph?channel=${i}&arch=amd64" -o "streams/${i}.json" )
   mkdir -p streams/expanded
   cat "streams/${i}.json" | node .github/workflows/expand.js > "streams/expanded/${i}.json"
 
